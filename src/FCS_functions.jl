e8f13e774c67d87a109d78c0e0c9b9595a61da3f
Marcelo <marcelobro.pera@gmail.com>
2023-10-13 15:06:43 +0200

    Added function drazin, which computes the drazin inverse based on the Moore-Penrose inverse

--- src/FCS_functions.jl
+++ src/FCS_functions.jl
@@ -20,3 +20,25 @@ Calculate n-th zero-frequency cumulant of full counting statistics using a recur
 function fcscumulants_recursive(H::AbstractOperator, J, mJ, nCum; tol=1e-3, nu = vcat(-1*ones(length(J), ones(length(J)))))
     return nothing
 end
+"""
+   drazin(H, J; rho_ss = steadystate.eigenvector(H, J))
+   
+Calculate the Drazin inverse of a Liouvillian defined by the Hamiltonian H and jump operators J.
+
+# Arguments
+* `H`: Arbitrary operator specifying the Hamiltonian.
+* `J`: Vector containing all jump operators which can be of any arbitrary
+        operator type.
+* `rho_ss`: Density matrix specifying the steady-state of the Liouvillian. By default, it is found through steadystate.eigenvector. 
+        For large matrices the steady-state should be provided, as the best steady-state solver could vary.
+"""
+function drazin(H::AbstractOperator, J; rho_ss = steadystate.eigenvector(H, J))
+        d = length(H)
+        vId = reshape(Matrix(identityoperator(H).data), d)'
+        vss = reshape(Matrix(rho_ss.data), d)
+        vL = Matrix(L.data)
+        IdL = Matrix{ComplexF64}(I, d, d)
+        Q = IdL - vss*vId
+        LD = Q*pinv(vL)*Q
+        return LD
+    end
--- src/QuantumFCS.jl
+++ src/QuantumFCS.jl
@@ -2,4 +2,5 @@ module QuantumFCS
     using QuantumOptics
     include("FCS_functions.jl")
     export fcscumulants_recursive
+    export drazin
 end
