46ea7a4797facaea9711f0866238010e75febfc6
Marcelo <marcelobro.pera@gmail.com>
2025-09-25 14:24:28 +0200

    Deleted spurious extra ext file.

--- ext/ QuantumFCSQuantumOpticsExt.jl
+++
@@ -1,6 +0,0 @@
-module QuantumFCSQuantumOpticsExt
-    using QuantumFCS
-    import QuantumOptics
-    # Methods that *dispatch on QuantumOptics types* live here:
-    include("FCS_QuantumOptics_functions.jl")
-end
